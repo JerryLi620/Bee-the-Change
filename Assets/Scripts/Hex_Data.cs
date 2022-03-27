2fdafc32440442ee16e9b24c464edee88a04b64f
Jerry Li <lmyjerry@gmail.com>
2022-03-27 17:12:37 -0400

    Hex_Data

---
+++ Assets/Scripts/Hex_Data.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex_Data : MonoBehaviour
+{
+    public int population = 0;
+    public int HoneyProduction = 0;
+    public int BeeRights = 0;
+    public int FlowerDensity = 0;
+    public int LifeExpectancy = 0;
+    public int Happiness = 0;
+}
