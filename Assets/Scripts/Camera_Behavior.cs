4bc6250ac6c510f0e5f45cc1c751d3759f21dabe
Jack Barnett <jabarnett@davidson.edu>
2022-03-28 17:46:55 -0400

    Camera zoom on click

---
+++ Assets/Scripts/Camera_Behavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Behavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Camera.main.orthographicSize = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      if ((Input.GetMouseButtonDown(0)) && (Camera.main.orthographicSize != 1.3f))
+        Camera.main.orthographicSize = 1.3f;
+      else if(Input.GetMouseButtonDown(0))
+        Camera.main.orthographicSize = 5.0f;
+      //if (Input.GetMouseButton(0))
+      //{
+    	   //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+     	   //RaycastHit hit;
+     	// Casts the ray and get the first game object hit
+     	  //Physics.Raycast(ray, out hit);
+     	  //Debug.Log("This hit at " + hit.point );
+      //}
+    }
+}
