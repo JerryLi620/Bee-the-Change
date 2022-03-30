3d50e3b771e2bd4364738169cc9143d298c0222d
Jack Barnett <jabarnett@davidson.edu>
2022-03-29 21:46:46 -0400

    Zooming functionality added

--- Assets/Scripts/Click_Detection.cs
+++ Assets/Scripts/Click_Detection.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class Click_Detection : MonoBehaviour
 {
+  public bool zoomedIn;
+
+  void Start(){
+    zoomedIn = false;
+  }
+
   void Update(){
     if (Input.GetMouseButtonDown (0)) {
+      if(zoomedIn==false){
              var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
              RaycastHit hit;
 
              if (Physics.Raycast(ray, out hit, 100)) {
-                 Debug.Log(hit.collider.gameObject.transform.parent.name);
+                 Camera.main.transform.position = hit.collider.gameObject.transform.parent.position;
+                 Camera.main.transform.position= Camera.main.transform.position + Vector3.up;
+                 Camera.main.orthographicSize = 1.3f;
+                 zoomedIn=true;
              }
          }
+         else{
+           Camera.main.orthographicSize = 6.0f;
+           Camera.main.transform.position = Vector3.up;
+           zoomedIn=false;
+      }
+      }
   }
 }
