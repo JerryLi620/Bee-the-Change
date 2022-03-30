6e7abf74ec74e88b764b076a0c63c5cd0e7a80f6
Paige Johnson <pajohnson@davidson.edu>
2022-03-29 22:09:39 -0400

    adding popularity tracker

--- Assets/Scripts/DataDisplay.cs
+++ Assets/Scripts/DataDisplay.cs
@@ -12,12 +12,14 @@ public class DataDisplay : MonoBehaviour
     private int flowerDensity = 10;
     private int lifeExpectancy = 10;
     private int overallHappiness = 10;
+    private int popularity = 100;
 
     public TMP_Text honeyText;
     public TMP_Text beeText;
     public TMP_Text flowerText;
     public TMP_Text lifeText;
     public TMP_Text happinessText;
+    public TMP_Text popularityText;
 
     // Update is called once per frame
     void Update()
@@ -27,6 +29,7 @@ public class DataDisplay : MonoBehaviour
         flowerText.text = "Flower Density : " + flowerDensity;
         lifeText.text = "Life Expectancy : " + lifeExpectancy;
         happinessText.text = "Overall Happiness : " + overallHappiness;
+        popularityText.text = "Popularity : " + popularity;
 
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -36,6 +39,7 @@ public class DataDisplay : MonoBehaviour
             flowerDensity--;
             lifeExpectancy--;
             overallHappiness--;
+            popularity--;
         }
     }
 }
