c1bdf84689d550a11a728d41c5fd4e1a55f070a7
Jack Barnett <jabarnett@davidson.edu>
2022-04-21 12:44:36 -0400

    Revert "Remove old scripts"
    This reverts commit 5c59e368fc0a53f14cd499187519638e8c02f299.

--- Assets/Scripts/Hex_Data.cs
+++ Assets/Scripts/Hex_Data.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Hex_Data : MonoBehaviour
 {
+    public string topTrait;
 
 void Start()
 {
+int random_number = Random.Range(1, 7);
+  if(random_number == 1){
+    topTrait = "honeyProduction";
+  }
+  if(random_number == 2){
+    topTrait = "beeRights";
+  }
+  if(random_number == 3){
+    topTrait = "flowerDensity";
+  }
+  if(random_number == 4){
+    topTrait = "lifeExpectancy";
+  }
+  if(random_number == 5){
+    topTrait = "happiness";
+}
 
 }
 }
