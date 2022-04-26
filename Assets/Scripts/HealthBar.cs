d66a89c911431e49b0b7a34ffd6295e0ca7d07b4
Paige Johnson <pajohnson@davidson.edu>
2022-04-26 09:29:04 -0400

    making all 5 bars

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,22 +5,63 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public Slider popSlider;
     public Slider honeySlider;
+    public Slider brSlider;
+    public Slider flowerSlider;
+    public Slider lifeSlider;
+    public Slider hapSlider;
     public Gradient gradient;
     public Image fill;
 
-    public void SetMaxHealth(int stat)
+    public void SetMaxHealth(double stat)
     {
         honeySlider.maxValue = stat;
-        honeySlider.value = stat;
+        honeySlider.value = stat / 2;
+
+        popSlider.maxValue = stat;
+        popSlider.value = stat / 2;
+
+        brSlider.maxValue = stat;
+        brSlider.value = stat / 2;
+
+        flowerSlider.maxValue = stat;
+        flowerSlider.value = stat / 2;
+
+        lifeSlider.maxValue = stat;
+        lifeSlider.value = stat / 2;
+
+        hapSlider.maxValue = stat;
+        hapSlider.value = stat / 2;
 
         fill.color = gradient.Evaluate(1f);
     }
 
-    public void SetHealth(int stat)
+    public void Start()
+    {
+        SetMaxHealth(100.0);
+    }
+
+    public void SetHealth(double stat)
     {
         honeySlider.value = stat;
         fill.color = gradient.Evaluate(honeySlider.normalizedValue);
+
+        popSlider.value = stat;
+        fill.color = gradient.Evaluate(popSlider.normalizedValue);
+
+        brSlider.value = stat;
+        fill.color = gradient.Evaluate(brSlider.normalizedValue);
+
+        flowerSlider.value = stat;
+        fill.color = gradient.Evaluate(flowerSlider.normalizedValue);
+
+        lifeSlider.value = stat;
+        fill.color = gradient.Evaluate(lifeSlider.normalizedValue);
+
+        hapSlider.value = stat;
+        fill.color = gradient.Evaluate(hapSlider.normalizedValue);
+
     }
 
 }
--- Assets/Scripts/UpdateBar.cs
+++ Assets/Scripts/UpdateBar.cs
@@ -6,30 +6,50 @@ using System;
 
 public class UpdateBar : MonoBehaviour
 {
-    public int currentStat;
+    public double popStat;
+    public double honeyStat;
+    public double brStat;
+    public double flowerStat;
+    public double lifeStat;
+    public double hapStat;
 
-    public HealthBar healthBar;
+    public HealthBar popBar;
+    public HealthBar honeyBar;
+    public HealthBar brBar;
+    public HealthBar flowerBar;
+    public HealthBar lifeBar;
+    public HealthBar hapBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        healthBar.SetHealth(50);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(currentStat != Map_Behavior.honeyProduction)
-        {
-           healthBar.SetHealth(Map_Behavior.honeyProduction);
-        }
-   
+        popBar.SetHealth(50.0d);
+        honeyBar.SetHealth(50.0d);
+        brBar.SetHealth(50.0d);
+        flowerBar.SetHealth(50.0d);
+        lifeBar.SetHealth(50.0d);
+        hapBar.SetHealth(50.0d);
     }
 
     void Update()
     {
-        currentHealth = Map_Behavior.honeyProduction ;
-        healthBar.SetHealth(currentHealth);
+        honeyStat = Map_Behavior.honeyProduction;
+        honeyBar.SetHealth(honeyStat);
+
+        popStat = Map_Behavior.popularity;
+        popBar.SetHealth(popStat);
+
+        brStat = Map_Behavior.beeRights;
+        brBar.SetHealth(brStat);
+
+        flowerStat = Map_Behavior.flowerDensity;
+        flowerBar.SetHealth(flowerStat);
+
+        lifeStat = Map_Behavior.lifeExpectancy;
+        lifeBar.SetHealth(lifeStat);
+
+        hapStat = Map_Behavior.happiness;
+        hapBar.SetHealth(hapStat);
     }
 
 }
